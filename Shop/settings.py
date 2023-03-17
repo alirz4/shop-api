da162fe85b536dfbe7cffc6928bcc74c87afbc15
Alireza Keshavarz <alir4akeshavrz@gmail.com>
2023-03-17 06:47:31 +0330

    add media_root in setting.py

--- Shop/settings.py
+++ Shop/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/medias/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
